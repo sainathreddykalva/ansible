ce170a043cecf9042a94c49c8c51c9cf4c080049
sainath <kalvasainathreddy@gmail.com>
2021-06-13 19:05:32 +0530

    updated host zone id

--- instance-launch/launch.sh
+++ instance-launch/launch.sh
@@ -16,7 +16,7 @@ LVER=4
 DNS_UPDATE() {
   PRIVATEIP=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1)
   sed -e "s/COMPONENT/$COMPONENT/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z05978542SSEPRN2TUR0E --change-batch file:///tmp/record.json | jq
+  aws route53 change-resource-record-sets --hosted-zone-id Z001659326LFQL66EVHGM --change-batch file:///tmp/record.json | jq
 }
   INSTANCE_CREATE() {
   INSTANCE_STATE=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].State.Name | xargs -n1)
